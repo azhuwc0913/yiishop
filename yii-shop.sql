e7c649f68602db9acff437728f09845b38d78d65
lawrence <lawrence@sasha-lab.com>
2016-07-30 14:49:12 +0800

    sql

--- yii-shop.sql
+++ yii-shop.sql
@@ -1,7 +1,7 @@
-git/*
+/*
 Navicat MySQL Data Transfer
 
-Source Server         : lawrence
+Source Server         : localhost_3306
 Source Server Version : 50538
 Source Host           : localhost:3306
 Source Database       : yii-shop
@@ -10,10 +10,79 @@ Target Server Type    : MYSQL
 Target Server Version : 50538
 File Encoding         : 65001
 
-Date: 2016-07-29 21:53:59
+Date: 2016-07-30 14:48:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `admin`
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `role` enum('user','admin') COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES ('1', 'admin11', 'BRsSNd8dp6U', '$2y$13$TLsdsv6cvgFVp0wmnPjZf.OUzbpWcQB6v.u.8DjiXXx1hfT3xipEy', null, 'admin@123.com', '10', '1469698521', '1469698521', 'admin');
+
+-- ----------------------------
+-- Table structure for `auth_assignment`
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_assignment`;
+CREATE TABLE `auth_assignment` (
+  `item_name` varchar(64) NOT NULL,
+  `user_id` varchar(64) NOT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`item_name`,`user_id`),
+  CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_assignment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `auth_item`
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_item`;
+CREATE TABLE `auth_item` (
+  `name` varchar(64) NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text,
+  `rule_name` varchar(64) DEFAULT NULL,
+  `data` text,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  KEY `rule_name` (`rule_name`),
+  KEY `type` (`type`),
+  CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_item
+-- ----------------------------
+INSERT INTO `auth_item` VALUES ('[admin/create]', '2', '创建管理员', null, null, '1469799394', '1469799394');
+INSERT INTO `auth_item` VALUES ('[admin/index]', '2', '创建[[admin/index]]权限', null, null, '1469775012', '1469775012');
+INSERT INTO `auth_item` VALUES ('[admin/update]', '2', '后台管理员的更新操作', null, null, '1469778974', '1469778974');
+INSERT INTO `auth_item` VALUES ('[item/all]', '2', '创建[[item/all]]权限', null, null, '1469778992', '1469778992');
+INSERT INTO `auth_item` VALUES ('管理员管理1', '1', '管理员管理1', null, null, '1469781852', '1469799459');
 
 -- ----------------------------
 -- Table structure for `auth_item_child`
@@ -34,3 +103,60 @@ CREATE TABLE `auth_item_child` (
 INSERT INTO `auth_item_child` VALUES ('管理员管理1', '[admin/create]');
 INSERT INTO `auth_item_child` VALUES ('管理员管理1', '[admin/index]');
 INSERT INTO `auth_item_child` VALUES ('管理员管理1', '[admin/update]');
+
+-- ----------------------------
+-- Table structure for `auth_rule`
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_rule`;
+CREATE TABLE `auth_rule` (
+  `name` varchar(64) NOT NULL,
+  `data` text,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_rule
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `migration`
+-- ----------------------------
+DROP TABLE IF EXISTS `migration`;
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of migration
+-- ----------------------------
+INSERT INTO `migration` VALUES ('m000000_000000_base', '1469697406');
+INSERT INTO `migration` VALUES ('m130524_201442_init', '1469697413');
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', 'ZeuOmVsQYU5aiCvSOwA5-BRsSNd8dp6U', '$2y$13$TLsdsv6cvgFVp0wmnPjZf.OUzbpWcQB6v.u.8DjiXXx1hfT3xipEy', null, 'admin@123.com', '10', '1469698521', '1469698521');
